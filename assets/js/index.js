61e69c550dfbdef190ffb08171be19e7a61291cd
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2021-07-21 02:31:43 -0600

    Se establecio el carrusel de películas en preventa

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,14 @@
         } 
     });  
   });
+
+  // JavaScript Document
+$(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
