ac81d4499da999a73da653c6331d27c13f1e7275
Luis Garcia <luis_garciamnz@hotmail.com>
2021-08-03 02:03:53 -0600

    Se linkeo cartelera.php en todas las páginas

--- promociones.php
+++ promociones.php
@@ -35,7 +35,7 @@
               <a class="nav-link" aria-current="page" href="#">PELÍCULAS</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">CARTELERA</a>
+              <a class="nav-link" href="cartelera.php">CARTELERA</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="promociones.php" tabindex="-1" aria-disabled="true">PROMOCIONES</a>
