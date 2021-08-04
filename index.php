a463b648dabe505d8798842d627bf8dd62ec0b9e
Luis Garcia <luis_garciamnz@hotmail.com>
2021-08-03 20:58:40 -0600

    Se linkeo peliculas.php en todas las paginas

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
             <div class="mb-0 collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                     <a class="nav-link" aria-current="page" href="pelicula.php">PELÍCULAS</a>
+                     <a class="nav-link" aria-current="page" href="peliculas.php">PELÍCULAS</a>
                   </li>
                   <li class="nav-item">
                      <a class="nav-link" href="cartelera.php">CARTELERA</a>
--- peliculas.php
+++ peliculas.php
@@ -50,10 +50,10 @@
             <div class="mb-0 collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                     <a class="nav-link" aria-current="page" href="#">PELÍCULAS</a>
+                     <a class="nav-link" aria-current="page" href="peliculas.php">PELÍCULAS</a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="#">CARTELERA</a>
+                     <a class="nav-link" href="cartelera.php">CARTELERA</a>
                   </li>
                   <li class="nav-item">
                      <a class="nav-link" href="promociones.php" tabindex="-1" aria-disabled="true">PROMOCIONES</a>
