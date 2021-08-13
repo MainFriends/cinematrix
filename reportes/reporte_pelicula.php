5c6ea90873af5924b3dcd6a65c6df68b69212e54
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2021-08-13 02:25:43 -0600

    Diseño reporte películas

--- reportes/reporte_pelicula.php
+++ reportes/reporte_pelicula.php
@@ -23,39 +23,53 @@ function Header()
     //Logo
     $this->Image('../assets/img/logos/logo-reporte.jpg',10,8,33);
     // Arial bold 15
-    $this->SetFont('Arial','B',15);
+    $this->SetFont('Arial','I',12);
     // Movernos a la derecha
     $this->Cell(65);
     // Título
-    $this->Cell(60,30,utf8_decode('Reporte de Películas'),0,0,'C');
+    $this->Cell(60,30,utf8_decode('REPORTE DE PELICULAS'),0,0,'C');
     // Salto de línea
     $this->Ln(30);
+    // Encabezados
+    $this->SetFillColor(151,148,159);
+    $this->Cell(38,8,utf8_decode('Películas'),0,0,'C',1);
+    $this->Cell(38,8,utf8_decode('Estado'),0,0,'C',1);
+    $this->Cell(38,8,utf8_decode('Género'),0,0,'C',1);
+    $this->Cell(38,8,utf8_decode('Clasificación'),0,0,'C',1);
+    $this->Cell(38,8,utf8_decode('Año'),0,1,'C',1);
+    $this->Ln(3);
 }
+
 
 // Pie de página
 function Footer()
 {
+    // Color
+    $this->SetFillColor(31, 73, 99);
+    $this->Rect(0, 280, 220, 50, 'F');
     // Posición: a 1,5 cm del final
     $this->SetY(-15);
     // Arial italic 8
-    $this->SetFont('Arial','I',8);
+    $this->SetFont('Arial','I',11);
     // Número de página
-    $this->Cell(0,10,utf8_decode('Reporte').$this->PageNo().'/{nb}',0,0,'C');
+    $this->SetX(120);
+    $this->SetTextColor(255,255,255);
+    $this->Cell(0,10,utf8_decode('Dónde la ficción se convierte en realidad ').$this->PageNo().'/{nb}',0,0,'C');
 }
-
 }
 
 $pdf = new PDF();
 $pdf -> AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',10);
+$pdf->SetFont('Arial','I',10);
+$pdf->SetMargins(10,30,20,20);
 
 foreach($data as $peli){
-    $pdf->Cell(38,13,$peli['TITULO'],1,0,'C',0);
-    $pdf->Cell(38,13,$peli['AÑO'],1,0,'C',0);
-    $pdf->Cell(38,13,utf8_decode($peli['GENERO']),1,0,'C',0);
-    $pdf->Cell(38,13,utf8_decode($peli['CLASIFICACION']),1,0,'C',0);
-    $pdf->Cell(38,13,utf8_decode($peli['DESCRIPCION']),1,1,'C',0);
+    $pdf->Cell(38,8,$peli['TITULO'],0,0,'C',0);
+    $pdf->Cell(38,8,utf8_decode($peli['DESCRIPCION']),0,0,'C',0);
+    $pdf->Cell(38,8,utf8_decode($peli['GENERO']),0,0,'C',0);
+    $pdf->Cell(38,8,utf8_decode($peli['CLASIFICACION']),0,0,'C',0);
+    $pdf->Cell(38,8,($peli['AÑO']),0,1,'C',0);
 }
 
 $pdf->Output();
