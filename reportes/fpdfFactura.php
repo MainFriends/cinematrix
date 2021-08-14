51a2c8b0743b71c018b98425c58cf22a13826e0f
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2021-08-14 03:01:22 -0600

    Update de reporte factura

--- reportes/fpdfFactura.php
+++ reportes/fpdfFactura.php
@@ -25,10 +25,22 @@ session_start();
     FROM DETALLE, BOLETO
     WHERE ID_FACTURA = '$idFactura'
     AND DETALLE.ID_BOLETO = BOLETO.ID_BOLETO
-    AND DETALLE.ID_BOLETO = 1;";
+    AND DETALLE.ID_BOLETO = 1";
     $statement = $conexion->prepare($query);
     $statement->execute();
     $boleto1 = $statement->fetch(PDO::FETCH_ASSOC);
+
+    //MOSTRAR DETALLE BOLETO 2
+    $query = "SELECT BOLETO.NOMBRE NOMBRE_BOLETO, ROUND(COUNT(BOLETO.ID_BOLETO)/2,0) CANTIDAD, PRECIO, ROUND(COUNT(BOLETO.ID_BOLETO)/2,0)*PRECIO TOTAL
+    FROM DETALLE, BOLETO
+    WHERE ID_FACTURA = '$idFactura'
+    AND DETALLE.ID_BOLETO = BOLETO.ID_BOLETO
+    AND DETALLE.ID_BOLETO = 2;";
+    $statement = $conexion->prepare($query);
+    $statement->execute();
+    $boleto2 = $statement->fetch(PDO::FETCH_ASSOC);
+
+    
 
     //MOSTRA COMBOS QUE COMPRO EL USUARIO
     $query = "SELECT COMBO.NOMBRE NOMBRE_COMBO, COUNT(DETALLE.ID_COMBO) CANTIDAD, COMBO.PRECIO, COUNT(DETALLE.ID_COMBO)*PRECIO TOTAL
@@ -54,18 +66,9 @@ function Header()
     $this->Cell(62);
     //Titulo
     $this->Cell(60,50,utf8_decode('CINEMATRIX'),0,0,'C');
-    $this->Ln(3);
+    $this->Ln(5);
     // Salto de línea
     $this->Ln(30);
-    // Encabezados
-    $this->SetFillColor(255,255,255);
-    $this->Cell(37,8,utf8_decode('Uds'),0,0,'C',1);
-    // Movernos a la derecha
-    $this->Cell(-15);
-    $this->Cell(40,8,utf8_decode('Descripción'),0,0,'C',1);
-    $this->Cell(110,8,utf8_decode('Precio'),0,0,'C',1);
-    $this->Cell(-35,8,utf8_decode('Total'),0,1,'C',1);
-    $this->Ln(3);
 }
 
 
@@ -89,9 +92,54 @@ function Footer()
 $pdf = new PDF();
 $pdf -> AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','I',10);
+$pdf->SetFont('Courier','',10);
 $pdf->SetMargins(10,30,20,20);
 
+        $pdf->SetFont('Courier','B',12); 
+        // Encabezados
+        $pdf->SetFillColor(255,255,255);
+        $pdf->Cell(37,8,utf8_decode('Uds'),0,0,'C',1);
+        // Movernos a la derecha
+        $pdf->Cell(-15);
+        $pdf->Cell(40,8,utf8_decode('Descripción'),0,0,'C',1);
+        $pdf->Cell(110,8,utf8_decode('Precio'),0,0,'C',1);
+        $pdf->Cell(-35,8,utf8_decode('Total'),0,1,'C',1);
+        $pdf->Ln(3);
+
+        $pdf->SetFont('Courier','B',12); 
+        // Movernos a la derecha
+        $pdf->Cell(13); 
+        $pdf->Cell(38,8,'Entradas',0,1,'L',0);
+        $pdf->Ln(3);
+ 
+    // Entradas
+    $pdf->SetFont('Courier','',12); 
+    if($boleto1['CANTIDAD']>0){
+        //boleto 1
+        $pdf->Cell(38,8,$boleto1['CANTIDAD'],0,0,'C',0);
+        $pdf->Cell(-10);
+        $pdf->Cell(70,8,$boleto1['NOMBRE_BOLETO'],0,0,'L',0);
+        $pdf->Cell(38,8,$boleto1['PRECIO'],0,0,'C',0);
+        $pdf->Cell(38,8,$boleto1['TOTAL'],0,1,'C',0);
+    }
+    if($boleto2['CANTIDAD']>0){
+        //boleto 2
+        $pdf->Cell(38,8,$boleto2['CANTIDAD'],0,0,'C',0);
+        $pdf->Cell(-10);
+        $pdf->Cell(70,8,$boleto2['NOMBRE_BOLETO'],0,0,'L',0);
+        $pdf->Cell(38,8,$boleto2['PRECIO'],0,0,'C',0);
+        $pdf->Cell(38,8,$boleto2['TOTAL'],0,1,'C',0);
+    }
+    
+
+
+    $pdf->SetFont('Courier','B',12); 
+    // Movernos a la derecha
+    $pdf->Cell(13); 
+    $pdf->Cell(38,8,'Alimentos y Bebidas',0,1,'L',0);
+    $pdf->Ln(3);
+    
+    $pdf->SetFont('Courier','',12); 
     foreach($combos as $combo){
      $pdf->Cell(38,8,$combo['CANTIDAD'],0,0,'C',0);
      $pdf->Cell(-10);
