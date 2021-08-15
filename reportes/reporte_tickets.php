2969e87782ad333b53308c32680b0aab66fc8146
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2021-08-15 02:59:55 -0600

    Reporte avanze tickets

--- reportes/reporte_tickets.php
+++ reportes/reporte_tickets.php
@@ -29,6 +29,36 @@ AND IDIOMA.ID_IDIOMA = CARTELERA.ID_IDIOMA
 AND CLASIFICACION.ID_CLASIFICACION = PELICULA.ID_CLASIFICACION
 AND FORMATO.ID_FORMATO = CARTELERA.ID_FORMATO
 AND DETALLE.ID_BOLETO = BOLETO.ID_BOLETO
-AND ID_FACTURA = 2";
+AND ID_FACTURA = $idFactura";
+$statement = $conexion->prepare($query);
+$statement->execute();
+$ticket = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+class PDF extends FPDF{
+
+
+function Header()
+{
+
+}
+}
+
+
+
+$pdf = new PDF();
+$pdf -> AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Courier','',10);
+
+foreach($ticket as $boletos){
+    $pdf->SetFont('Courier','B',12);
+    $pdf->SetMargins(5,5,5,5);
+    $pdf->Cell(65);
+    //Titulo
+    $pdf->Cell(100,8,utf8_decode('MULTIPLAZA CINEMATRIX'),0,0,'C');
+    $pdf->Ln(3);
+}
+
+$pdf->Output();
 
 ?>
