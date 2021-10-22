6de01d6dfe455a2b461285b0a3dd06c123f49a76
Luis Garcia <luis_garciamnz@hotmail.com>
2021-10-21 22:01:58 -0600

    Se modifico el archivo login.php

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cinematrix - Inicia sesión o regístrate</title>
+    <title>Cinematrix - Inicia sesión o regístrate por favor</title>
     <link rel="shortcut icon" href="assets/img/logos/cinematrix_ico.png">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
